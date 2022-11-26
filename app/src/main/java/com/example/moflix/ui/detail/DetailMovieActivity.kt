1d02554173b72e8399e6adf966e0510797bfdb4e
dbesar <dededarirahmadi@gmail.com>
2022-11-26 23:53:30 +0700

    modify detail activity

--- app/src/main/java/com/example/moflix/ui/detail/DetailMovieActivity.kt
+++ app/src/main/java/com/example/moflix/ui/detail/DetailMovieActivity.kt
@@ -1,16 +1,20 @@
 package com.example.moflix.ui.detail
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.annotation.VisibleForTesting
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.core_model.Movie
 import com.example.core_model.MovieDetail
 import com.example.moflix.R
 import com.example.moflix.databinding.ActivityDetailMovieBinding
+import com.example.moflix.ui.main.MainActivity
 import com.skydoves.bindables.BindingActivity
+import com.skydoves.bundler.Bundler
 import com.skydoves.bundler.bundleNonNull
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -37,6 +41,13 @@ class DetailMovieActivity : BindingActivity<ActivityDetailMovieBinding>(R.layout
         @VisibleForTesting
         internal const val EXTRA_MOVIE = "EXTRA_MOVIE"
 
+        fun runActivity(context: Context, movie: Movie) {
+            val intent = Intent(context, DetailMovieActivity::class.java)
+            val bundle = Bundle().apply {
+                putParcelable(EXTRA_MOVIE, movie)
+            }
+            startActivity(context, intent, bundle)
+        }
 
     }
 }
